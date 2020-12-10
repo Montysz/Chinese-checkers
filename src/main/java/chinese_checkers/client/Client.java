3a7f9b34140cf14f23c810166cbe9d4c3c15cb2d
montysz <montysz@DESKTOP-TC333FO>
2020-12-10 18:12:25 +0100

    Add Draw class

--- src/main/java/chinese_checkers/client/Client.java
+++ src/main/java/chinese_checkers/client/Client.java
@@ -1,5 +1,8 @@
 package chinese_checkers.client;
+
 //class of Client
 public class Client {
 int a;
 }
+
+
---
+++ src/main/java/chinese_checkers/client/Draw.java
@@ -0,0 +1,153 @@
+package chinese_checkers.client;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import javax.swing.JFrame;
+
+import chinese_checkers.Exceptions.wrongNumberOfPlayersException;
+import chinese_checkers.serwer.Board;
+import chinese_checkers.serwer.Tile;
+//class of Client
+
+public class Draw extends JFrame implements MouseListener{
+
+
+	private int numberOfPlayers = 6;
+	//try catch
+	//Board board = new Board(numberOfPlayers);
+
+
+	private static Board Tmp;
+//	Tile a = Tmp.gameBoard[1][2];
+
+	//zwraca int
+//	int b = Tmp.gameBoard[1][2].getX();
+
+	public Draw() {
+		
+		   setSize(500, 500);
+	       
+	       
+	        addMouseListener(this);
+	}
+
+    public static void main(String[] args) 
+    {
+    	try {
+    		Tmp = new Board(6);
+    	}
+    	catch(wrongNumberOfPlayersException e) {
+    		
+    	}
+
+    	Draw draw = new Draw();
+       draw.setVisible(true);
+       draw.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    public void paint(Graphics g)
+    {
+        drawBoard(g, Tmp);
+    }
+    
+    public void drawLine(Graphics2D g2)
+    {
+        for(int j=0; j<9; j++)
+            {
+                g2.setColor(Color.red);
+                g2.fillRect(50, 50, 407, 407);
+            }
+        
+    }
+    
+    public void drawBoard(Graphics g, Board gameboard)
+    { 	
+    	Image img= createImage(getSize().width, getSize().height);
+        Graphics2D g2= (Graphics2D)img.getGraphics();
+        drawLine(g2);
+        
+        for(int i=0;i<17;i++)
+        {
+            for(int j=0; j<13;j++)
+            {
+                
+                switch(gameboard.gameBoard[i][j].getPlayerId())
+                {
+                    case -1:
+                        g2.setColor(Color.black);
+                        g2.fillRect(j+50+j*50, i+50+i*50, 50, 50);
+                        break;
+                    case 1:
+                        g2.setColor(Color.red); 
+                         
+                 
+                            
+                        g2.fillRect(j+50+j*50, i+50+i*50, 50, 50);
+                        break;  
+                    case 2:
+                        g2.setColor(Color.black);
+                        g2.fillRect(j+50+j*50, i+50+i*50, 50, 50);
+                        g2.setColor(Color.red);                                     
+                        break;
+                    case 3:
+                        g2.setColor(Color.black);
+                        g2.fillRect(j+50+j*50, i+50+i*50, 50, 50);
+                        g2.setColor(Color.LIGHT_GRAY);
+                        break;
+                    case 4:
+                        g2.setColor(Color.blue);
+                        g2.fillRect(j+50+j*50, i+50+i*50, 50, 50);
+                        g2.setColor(Color.red);                                             
+                        break;    
+                }
+            }
+        }
+    }
+        public void Field(int x, int y)
+      {
+        if(x > 50 && x < 450 && y > 50 && y < 450)
+          {               
+                //xSize= y/50-1;
+                //ySize=x/50-1;                
+            //17 = y/50-1;
+            //13 = ySize=x/50-1;   
+          }
+        }  
+
+   
+    public void mouseClicked1(MouseEvent e) 
+    {
+        Field(e.getX(), e.getY());       
+        repaint();
+        
+    }	 
+    public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
+
+
